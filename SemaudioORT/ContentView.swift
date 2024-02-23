e9bc2717cec443beb2f580811b9d19ed3c3203d4
Andrew Nakamoto <andrewsnakamoto@gmail.com>
2024-02-22 18:07:59 -0800

    first pass at Model.swift, untested

--- SemaudioORT/ContentView.swift
+++ SemaudioORT/ContentView.swift
@@ -6,93 +6,43 @@
 //
 
 import SwiftUI
+import onnxruntime_objc
 
 struct ContentView: View {
   private let model = try! Model()
-  @State private var niter = 1
-  @State private var summary = "Waiting for input"
-  @State private var runtimes = "--"
-  @State private var average = "--"
-  @State private var std = "--"
-  @State private var computing = false
+  @State private var model_running = false
 
-  // runs the model and updates state to display results
-  private func compute_runtimes(_ niter: Int) async -> Void {
-      let times: [Double] = model.eval(niter: niter)
-      summary = "For \(niter) iterations:"
-      runtimes = "[" + times.map { String($0) }.joined(separator: ", ") + "]"
-      average = String(times.avg())
-      std = String(times.std())
-      print(runtimes)
+  var body: some View {
+    VStack {
+      Button(action: {
+        Task {
+          
+        }
+      }) {
+        Text("Enroll Placeholder")
+      }
+      Button(action: {
+        Task {
+          await toggle_tsh()
+          model_running = !model_running
+        }
+      }) {
+        if (model_running) {
+          Text("Turn off TSH")
+        } else {
+          Text("Begin TSH")
+        }
+      }
+    }.buttonStyle(.bordered)
   }
   
-  private func test_method() async -> Void {
-    let inputs = model.getInputs()
-    let outputs = model.runModel(inputs: inputs)
-    print(outputs)
+  private func toggle_tsh() async -> Void {
+    
   }
-
-    var body: some View {
-        VStack {
-            TextField("Enter a positive integer", value: $niter, formatter: NumberFormatter())
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
-            Button(action: {
-                computing = true
-                summary = "Model Running"
-                Task {
-                    await compute_runtimes(niter)
-                    computing = false
-                }
-            }) {
-                if (computing) {
-                    Text("Computing")
-                } else {
-                    Text("Compute Runtime")
-                }
-            }.disabled(computing)
-            Text("\(summary)")
-            Text("Average: \(average) ms")
-            Text("STD: \(std) ms")
-            Text("Runtimes: \(runtimes)")
-            Button(action: {
-                computing = true
-                summary = "Model Testing"
-                Task {
-                    await test_method()
-                    computing = false
-                }
-            }) {
-                if (computing) {
-                    Text("Testing")
-                } else {
-                    Text("Test")
-                }
-            }.disabled(computing)
-        }
-        .buttonStyle(.bordered)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-// helpers for calculating mean and std
-extension Array where Element: FloatingPoint {
-    func sum() -> Element {
-        return self.reduce(0, +)
-    }
-
-    func avg() -> Element {
-        return self.sum() / Element(self.count)
-    }
-
-    func std() -> Element {
-        let mean = self.avg()
-        let v = self.reduce(0, { $0 + ($1-mean)*($1-mean) })
-        return sqrt(v / (Element(self.count) - 1))
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- SemaudioORT/Model.swift
+++ SemaudioORT/Model.swift
@@ -6,6 +6,7 @@ import onnxruntime_objc
 class Model {
   private let ortEnv: ORTEnv
   private let ortSession: ORTSession
+  private var buffers: Dictionary<String, ORTValue>
 
   enum ModelError: Error {
     case Error(_ message: String)
@@ -20,158 +21,106 @@ class Model {
       throw ModelError.Error("Failed to find model file.")
     }
     ortSession = try ORTSession(env: ortEnv, modelPath: modelPath, sessionOptions: nil)
-  }
-
-  // Function to generate ORT tensor of a given shape with the option of initializing
-  // with either random values or zeros. 
-  func generateInputData(shape: [Int], random: Bool = false) -> ORTValue {
-      let inputData: [Float]
-      if random {
-          inputData = (0..<shape.reduce(1, *)) .map { _ in Float.random(in: -1...1) }
-      } else {
-          inputData = [Float](repeating: 0.0, count: shape.reduce(1, *))
-      }
-
-      // Convert inputData to Bytes
-      let inputDataBytes = Data (
-          bytes: inputData,
-          count: inputData.count * MemoryLayout<Float>.stride
-      )
-
-      let inputShape: [NSNumber] = shape.map { NSNumber(value: $0) }
-
-      // try to create ORTValue and hanle error
-      let input = try! ORTValue(
-          tensorData: NSMutableData(data: inputDataBytes),
-          elementType: ORTTensorElementDataType.float,
-          shape: inputShape)
     
-      return input
+    buffers = initializeInputs()
   }
   
-  func runModel(inputs: Dictionary<String, ORTValue>) -> Dictionary<String, ORTValue> {
+  // the important method
+  // returns the filtered output [1, 2, 128] when the current buffers are run through the model, and updates the buffers accordingly
+  func infer(mixture: ORTValue) -> ORTValue {
+    // update mixture
+    buffers["mixture"] = mixture
+    // run the model
     let outputs = try! ortSession.run(
-        withInputs: inputs,
+        withInputs: buffers,
         outputNames: ["filtered_output", "out::conv_buf", "out::deconv_buf", "out::gridnet_bufs::buf0::K_buf", "out::gridnet_bufs::buf0::V_buf", "out::gridnet_bufs::buf0::c0", "out::gridnet_bufs::buf0::h0", "out::gridnet_bufs::buf1::K_buf", "out::gridnet_bufs::buf1::V_buf", "out::gridnet_bufs::buf1::c0", "out::gridnet_bufs::buf1::h0", "out::gridnet_bufs::buf2::K_buf", "out::gridnet_bufs::buf2::V_buf", "out::gridnet_bufs::buf2::c0", "out::gridnet_bufs::buf2::h0", "out::istft_buf"],
         runOptions: nil)
-    return outputs
-  }
-  
-  func getInputs() -> Dictionary<String, ORTValue> {
-    let mixture = generateInputData(shape: [1, 2, 192], random: true)
-    let embedding = generateInputData(shape: [1, 1, 256], random: true) // constant
-    let conv_buf = generateInputData(shape: [1, 4, 2, 97], random: true)
-    let deconv_buf = generateInputData(shape: [1, 64, 2, 97], random: true)
-    let gridnet_bufs_buf0_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-    let gridnet_bufs_buf0_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-    let gridnet_bufs_buf0_c0 = generateInputData(shape: [1, 97, 64], random: true)
-    let gridnet_bufs_buf0_h0 = generateInputData(shape: [1, 97, 64], random: true)
-    let gridnet_bufs_buf1_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-    let gridnet_bufs_buf1_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-    let gridnet_bufs_buf1_c0 = generateInputData(shape: [1, 97, 64], random: true)
-    let gridnet_bufs_buf1_h0 = generateInputData(shape: [1, 97, 64], random: true)
-    let gridnet_bufs_buf2_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-    let gridnet_bufs_buf2_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-    let gridnet_bufs_buf2_c0 = generateInputData(shape: [1, 97, 64], random: true)
-    let gridnet_bufs_buf2_h0 = generateInputData(shape: [1, 97, 64], random: true)
-    let istft_buf = generateInputData(shape: [1, 2, 194, 1], random: true)
-    
-    let inputs = [
-      "mixture": mixture,
-      "embedding": embedding,
-      "conv_buf": conv_buf,
-      "deconv_buf": deconv_buf,
-      "gridnet_bufs::buf0::K_buf": gridnet_bufs_buf0_K_buf,
-      "gridnet_bufs::buf0::V_buf": gridnet_bufs_buf0_V_buf,
-      "gridnet_bufs::buf0::c0": gridnet_bufs_buf0_c0,
-      "gridnet_bufs::buf0::h0": gridnet_bufs_buf0_h0,
-      "gridnet_bufs::buf1::K_buf": gridnet_bufs_buf1_K_buf,
-      "gridnet_bufs::buf1::V_buf": gridnet_bufs_buf1_V_buf,
-      "gridnet_bufs::buf1::c0": gridnet_bufs_buf1_c0,
-      "gridnet_bufs::buf1::h0": gridnet_bufs_buf1_h0,
-      "gridnet_bufs::buf2::K_buf": gridnet_bufs_buf2_K_buf,
-      "gridnet_bufs::buf2::V_buf": gridnet_bufs_buf2_V_buf,
-      "gridnet_bufs::buf2::c0": gridnet_bufs_buf2_c0,
-      "gridnet_bufs::buf2::h0": gridnet_bufs_buf2_h0,
-      "istft_buf": istft_buf]
-    
-    return inputs
-  }
-
-
-  // Function to compute the runtime of the model in milliseconds averaged over
-  // `niters`` iterations.
-  func eval(niter: Int) -> [Double] {
-    var runtimes: [Double] = []
-
-    for _ in 0..<niter {
-        var runtime = 0.0
-        
-        let mixture = generateInputData(shape: [1, 2, 192], random: true)
-        let embedding = generateInputData(shape: [1, 1, 256], random: true) // constant
-        let conv_buf = generateInputData(shape: [1, 4, 2, 97], random: true)
-        let deconv_buf = generateInputData(shape: [1, 64, 2, 97], random: true)
-        let gridnet_bufs_buf0_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-        let gridnet_bufs_buf0_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-        let gridnet_bufs_buf0_c0 = generateInputData(shape: [1, 97, 64], random: true)
-        let gridnet_bufs_buf0_h0 = generateInputData(shape: [1, 97, 64], random: true)
-        let gridnet_bufs_buf1_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-        let gridnet_bufs_buf1_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-        let gridnet_bufs_buf1_c0 = generateInputData(shape: [1, 97, 64], random: true)
-        let gridnet_bufs_buf1_h0 = generateInputData(shape: [1, 97, 64], random: true)
-        let gridnet_bufs_buf2_K_buf = generateInputData(shape: [4, 49, 582], random: true)
-        let gridnet_bufs_buf2_V_buf = generateInputData(shape: [4, 49, 1552], random: true)
-        let gridnet_bufs_buf2_c0 = generateInputData(shape: [1, 97, 64], random: true)
-        let gridnet_bufs_buf2_h0 = generateInputData(shape: [1, 97, 64], random: true)
-        let istft_buf = generateInputData(shape: [1, 2, 194, 1], random: true)
-        
-        /*var filtered_output = shape.[1, 2, 128]
-        var out_conv_buf = [1, 4, 2, 97]
-        var out_deconv_buf = [1, 64, 2, 97]
-        var out_gridnet_bufs_buf0_K_buf = [4, 49, 582]
-        var out_gridnet_bufs_buf0_V_buf = [4, 49, 1552]
-        var out_gridnet_bufs_buf0_c0 = [1, 97, 64]
-        var out_gridnet_bufs_buf0_h0 = [1, 97, 64]
-        var out_gridnet_bufs_buf1_K_buf = [4, 49, 582]
-        var out_gridnet_bufs_buf1_V_buf = [4, 49, 1552]
-        var out_gridnet_bufs_buf1_c0 = [1, 97, 64]
-        var out_gridnet_bufs_buf1_h0 = [1, 97, 64]
-        var out_gridnet_bufs_buf2_K_buf = [4, 49, 582]
-        var out_gridnet_bufs_buf2_V_buf = [4, 49, 1552]
-        var out_gridnet_bufs_buf2_c0 = [1, 97, 64]
-        var out_gridnet_bufs_buf2_h0 = [1, 97, 64]
-        var out_istft_buf = [1, 2, 194, 1]*/
-
-        // Inference
-        let startTime = DispatchTime.now()
-        let _ = try! ortSession.run(
-            withInputs: [
-                "mixture": mixture,
-                "embedding": embedding,
-                "conv_buf": conv_buf,
-                "deconv_buf": deconv_buf,
-                "gridnet_bufs::buf0::K_buf": gridnet_bufs_buf0_K_buf,
-                "gridnet_bufs::buf0::V_buf": gridnet_bufs_buf0_V_buf,
-                "gridnet_bufs::buf0::c0": gridnet_bufs_buf0_c0,
-                "gridnet_bufs::buf0::h0": gridnet_bufs_buf0_h0,
-                "gridnet_bufs::buf1::K_buf": gridnet_bufs_buf1_K_buf,
-                "gridnet_bufs::buf1::V_buf": gridnet_bufs_buf1_V_buf,
-                "gridnet_bufs::buf1::c0": gridnet_bufs_buf1_c0,
-                "gridnet_bufs::buf1::h0": gridnet_bufs_buf1_h0,
-                "gridnet_bufs::buf2::K_buf": gridnet_bufs_buf2_K_buf,
-                "gridnet_bufs::buf2::V_buf": gridnet_bufs_buf2_V_buf,
-                "gridnet_bufs::buf2::c0": gridnet_bufs_buf2_c0,
-                "gridnet_bufs::buf2::h0": gridnet_bufs_buf2_h0,
-                "istft_buf": istft_buf],
-            outputNames: ["filtered_output", "out::conv_buf", "out::deconv_buf", "out::gridnet_bufs::buf0::K_buf", "out::gridnet_bufs::buf0::V_buf", "out::gridnet_bufs::buf0::c0", "out::gridnet_bufs::buf0::h0", "out::gridnet_bufs::buf1::K_buf", "out::gridnet_bufs::buf1::V_buf", "out::gridnet_bufs::buf1::c0", "out::gridnet_bufs::buf1::h0", "out::gridnet_bufs::buf2::K_buf", "out::gridnet_bufs::buf2::V_buf", "out::gridnet_bufs::buf2::c0", "out::gridnet_bufs::buf2::h0", "out::istft_buf"],
-            runOptions: nil)
-        let endTime = DispatchTime.now()
-
-        runtime += Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1e6
-        runtimes.append(runtime)
+    // update internal buffers
+    for name_ortvalue_pair in outputs {
+      if name_ortvalue_pair.key == "filtered_output" {
+        continue
+      }
+      var buffer_name: String = name_ortvalue_pair.key.replacingOccurrences(of: "out::", with: "")
+      buffers[buffer_name] = name_ortvalue_pair.value
     }
-
-    return runtimes
+    
+    // return
+    if let output = outputs["filtered_output"] {
+      return output
+    } else {
+      return generateORTValue(shape: [1, 2, 128], random: false)
+    }
   }
 
+}
+
+// STATIC HELPER FUNCTIONS
+
+// static function to get zeroed inputs for the model
+func initializeInputs() -> Dictionary<String, ORTValue> {
+  let mixture = generateORTValue(shape: [1, 2, 192], random: false)
+  let embedding = generateORTValue(shape: [1, 1, 256], random: false) // constant
+  let conv_buf = generateORTValue(shape: [1, 4, 2, 97], random: false)
+  let deconv_buf = generateORTValue(shape: [1, 64, 2, 97], random: false)
+  let gridnet_bufs_buf0_K_buf = generateORTValue(shape: [4, 49, 582], random: false)
+  let gridnet_bufs_buf0_V_buf = generateORTValue(shape: [4, 49, 1552], random: false)
+  let gridnet_bufs_buf0_c0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let gridnet_bufs_buf0_h0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let gridnet_bufs_buf1_K_buf = generateORTValue(shape: [4, 49, 582], random: false)
+  let gridnet_bufs_buf1_V_buf = generateORTValue(shape: [4, 49, 1552], random: false)
+  let gridnet_bufs_buf1_c0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let gridnet_bufs_buf1_h0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let gridnet_bufs_buf2_K_buf = generateORTValue(shape: [4, 49, 582], random: false)
+  let gridnet_bufs_buf2_V_buf = generateORTValue(shape: [4, 49, 1552], random: false)
+  let gridnet_bufs_buf2_c0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let gridnet_bufs_buf2_h0 = generateORTValue(shape: [1, 97, 64], random: false)
+  let istft_buf = generateORTValue(shape: [1, 2, 194, 1], random: false)
+  
+  let inputs = [
+    "mixture": mixture,
+    "embedding": embedding,
+    "conv_buf": conv_buf,
+    "deconv_buf": deconv_buf,
+    "gridnet_bufs::buf0::K_buf": gridnet_bufs_buf0_K_buf,
+    "gridnet_bufs::buf0::V_buf": gridnet_bufs_buf0_V_buf,
+    "gridnet_bufs::buf0::c0": gridnet_bufs_buf0_c0,
+    "gridnet_bufs::buf0::h0": gridnet_bufs_buf0_h0,
+    "gridnet_bufs::buf1::K_buf": gridnet_bufs_buf1_K_buf,
+    "gridnet_bufs::buf1::V_buf": gridnet_bufs_buf1_V_buf,
+    "gridnet_bufs::buf1::c0": gridnet_bufs_buf1_c0,
+    "gridnet_bufs::buf1::h0": gridnet_bufs_buf1_h0,
+    "gridnet_bufs::buf2::K_buf": gridnet_bufs_buf2_K_buf,
+    "gridnet_bufs::buf2::V_buf": gridnet_bufs_buf2_V_buf,
+    "gridnet_bufs::buf2::c0": gridnet_bufs_buf2_c0,
+    "gridnet_bufs::buf2::h0": gridnet_bufs_buf2_h0,
+    "istft_buf": istft_buf]
+  
+  return inputs
+}
+
+// Function to generate ORT tensor of a given shape with the option of initializing
+// with either random values or zeros.
+func generateORTValue(shape: [Int], random: Bool = false) -> ORTValue {
+  let inputData: [Float]
+  if random {
+    inputData = (0..<shape.reduce(1, *)) .map { _ in Float.random(in: -1...1) }
+  } else {
+    inputData = [Float](repeating: 0.0, count: shape.reduce(1, *))
+  }
+
+  // Convert inputData to Bytes
+  let inputDataBytes = Data (
+    bytes: inputData,
+    count: inputData.count * MemoryLayout<Float>.stride
+  )
+
+  let inputShape: [NSNumber] = shape.map { NSNumber(value: $0) }
+
+  // try to create ORTValue and handle error
+  let input = try! ORTValue(
+    tensorData: NSMutableData(data: inputDataBytes),
+    elementType: ORTTensorElementDataType.float,
+    shape: inputShape)
+
+  return input
 }
